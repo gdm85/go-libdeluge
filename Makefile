394f456973178eb1d8ccd0956e4e42e6dd9b4c3c
gdm85 <gdm85@users.noreply.github.com>
2016-08-18 08:48:41 +0200

    Fixed bug in EOF handling & added a test for it

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 build-all: build bin/add-torrent
 
-build:
+build: test
 	mkdir -p .gopath
 	if [ ! -L .gopath/src ]; then ln -s "$(CURDIR)/vendor" .gopath/src; fi
 	if [ ! -L .gopath/src/github.com/gdm85/go-libdeluge ]; then ln -s "$(CURDIR)" .gopath/src/github.com/gdm85/go-libdeluge; fi
 	GO15VENDOREXPERIMENT=1 GOPATH="$(CURDIR)/.gopath" go build
+
+test: *.go
+	go test
 
 bin/add-torrent: build
 	mkdir -p bin
@@ -13,4 +16,4 @@ bin/add-torrent: build
 clean:
 	rm -f bin/add-torrent
 
-.PHONY: build-all build clean
+.PHONY: build-all build clean test
---
+++ delugeclient_test.go
@@ -0,0 +1,54 @@
+package delugeclient
+
+/*
+ * go-libdeluge v0.1.0 - a native deluge RPC client library
+ * Copyright (C) 2015~2016 gdm85 - https://github.com/gdm85/go-libdeluge/
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+import (
+	"bytes"
+	"compress/zlib"
+	"fmt"
+	"io"
+	"testing"
+)
+
+func TestZlibEOF(t *testing.T) {
+	b := bytes.NewReader([]byte{120, 156, 59, 204, 200, 200, 5, 0, 3, 31, 0, 208})
+
+	zr, err := zlib.NewReader(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data := []byte{0}
+	var decomp []byte
+	for {
+		n, err := zr.Read(data)
+		if n == 1 {
+			decomp = append(decomp, data[0])
+		}
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Fatal(err)
+		}
+	}
+	const expected = "decomp = [195 1 1 10]\n"
+	s := fmt.Sprintln("decomp =", decomp)
+	if s != expected {
+		t.Fatalf("expected %q, got %q", expected, s)
+	}
+}
