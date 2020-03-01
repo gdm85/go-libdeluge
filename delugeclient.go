23a193f933400164379e35e34dca04dd7be69165
gdm85 <gdm85@users.noreply.github.com>
2020-03-01 16:27:48 +0100

    Deflate in a buffer to avoid too early rencode decoding
    Fixes #19

--- delugeclient.go
+++ delugeclient.go
@@ -366,7 +366,13 @@ func (c *Client) rpc(methodName string, args rencode.List, kwargs rencode.Dictio
 		return nil, err
 	}
 
-	d := rencode.NewDecoder(zr)
+	// buffered input here is necessary to avoid the rencode decoder racing past the ZLib decoder
+	var deflatedBuf bytes.Buffer
+	_, err = io.Copy(&deflatedBuf, zr)
+	if err != nil {
+		return nil, err
+	}
+	d := rencode.NewDecoder(&deflatedBuf)
 
 	resp, err := c.handleRpcResponse(d, c.serial)
 	if err != nil {
