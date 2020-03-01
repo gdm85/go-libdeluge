415e850c8852383af2880e2a0860116f378ea7bc
gdm85 <gdm85@users.noreply.github.com>
2020-03-01 16:47:13 +0100

    Version 0.4.0

--- delugeclient_test.go
+++ delugeclient_test.go
@@ -1,5 +1,5 @@
-// go-libdeluge v0.3.2 - a native deluge RPC client library
-// Copyright (C) 2015~2019 gdm85 - https://github.com/gdm85/go-libdeluge/
+// go-libdeluge v0.4.0 - a native deluge RPC client library
+// Copyright (C) 2015~2020 gdm85 - https://github.com/gdm85/go-libdeluge/
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
