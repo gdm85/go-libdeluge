fae643d0cca1a637f48ad82717365038955f0d1d
liut <liut@phoenixtv.com>
2021-04-01 18:12:47 +0800

    Implement GetSessionStatus()

---
+++ session_status.go
@@ -0,0 +1,53 @@
+package delugeclient
+
+import (
+	"github.com/gdm85/go-rencode"
+)
+
+// SessionStatus contains basic session status and statistics.
+type SessionStatus struct {
+	HasIncomingConnections int8
+	UploadRate             float32
+	DownloadRate           float32
+	PayloadUploadRate      float32
+	PayloadDownloadRate    float32
+	TotalDownload          int32
+	TotalUpload            int32
+	NumPeers               int16
+	DhtNodes               int16
+}
+
+// sessionStatusKeys is a slice with specific session status and statistics.
+var sessionStatusKeys = rencode.NewList(
+	"has_incoming_connections",
+	"upload_rate",
+	"download_rate",
+	"payload_upload_rate",
+	"payload_download_rate",
+	"total_download",
+	"total_upload",
+	"num_peers",
+	"dht_nodes",
+)
+
+// GetSessionStatus retrieves session status and statistics.
+func (c *Client) GetSessionStatus() (*SessionStatus, error) {
+	var args rencode.List
+	args.Add(sessionStatusKeys)
+
+	rd, err := c.rpcWithDictionaryResult("core.get_session_status", args, rencode.Dictionary{})
+	if err != nil {
+		return nil, err
+	}
+
+	if c.settings.Logger != nil {
+		c.settings.Logger.Printf("session status: %+s", rd)
+	}
+	var data SessionStatus
+	err = rd.ToStruct(&data, c.excludeV2tag)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
