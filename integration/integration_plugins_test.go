fefda1ff3c925302964281f2fd304f967109d5de
Jason Kingsbury <jason@relva.co.uk>
2021-12-23 23:53:23 +0000

    Simplify enable and disable plugin for integration test, remove asynchronous wait for plugin enabled

--- integration/integration_plugins_test.go
+++ integration/integration_plugins_test.go
@@ -5,55 +5,29 @@ package main
 import (
 	delugeclient "github.com/gdm85/go-libdeluge"
 	"testing"
-	"time"
 )
 
-func waitForPluginEnabled(t *testing.T, name string) {
-	tick := time.NewTicker(time.Millisecond * 500)
-	defer tick.Stop()
 
-	for attempt := 0; attempt < 10; attempt ++ {
-		// Sleep before testing enabled plugins.
-		<-tick.C
-
-		t.Logf("Attempt %d waiting for plugin %s to become enabled", attempt + 1, name)
-
-		plugins, err := c.GetEnabledPlugins()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		for _, p := range plugins {
-			if p == name {
-				return
-			}
-		}
-	}
-
-	t.Fatalf("Timeout waiting for plugin %s to become enabled", name)
-}
-
-func testWithPlugin(t *testing.T, name string) func() {
+func enablePlugin(t *testing.T, name string) {
 	err := deluge.EnablePlugin(name)
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
-	waitForPluginEnabled(t, name)
-
-	return func() {
-		err := deluge.DisablePlugin(name)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// cleanup DebugServerResponses after plugin disable
-		c.DebugServerResponses = nil
+func disablePlugin(t *testing.T, name string) {
+	err := deluge.DisablePlugin(name)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	// cleanup DebugServerResponses after plugin disable
+	c.DebugServerResponses = nil
 }
 
 func TestLabelPluginGetLabels(t *testing.T) {
-	defer testWithPlugin(t, "Label")()
+	enablePlugin(t, "Label")
+	defer disablePlugin(t, "Label")
 
 	var labelPlugin = &delugeclient.LabelPlugin{Client: c}
 
