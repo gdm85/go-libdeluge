d619c216d3d0afca9cc2aa46e2988047aa4e835b
gdm85 <gdm85@users.noreply.github.com>
2023-06-03 14:45:14 +0200

    Use Ubuntu20
    Ubuntu18 is now deprecated, so GitHub Actions will never run anything labelled for it

--- scripts/deluge-install.sh
+++ scripts/deluge-install.sh
@@ -23,22 +23,44 @@ set -e
 if [ "$1" = "--v2" ]; then
     export DEBIAN_FRONTEND="noninteractive"
 
-    apt-get install -qq -y gnupg
-
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C5E6A5ED249AD24C
-    echo "deb http://ppa.launchpad.net/deluge-team/stable/ubuntu bionic main" >> /etc/apt/sources.list.d/deluge.list
-
     ## NOTE: geoip-database is optional
     apt-get update
-    apt-get install -qq -y deluged geoip-database
+    apt-get install -qq -y deluged geoip-database python3-pip
 
-    ## necessary due to a recent regression, see also: https://forum.deluge-torrent.org/viewtopic.php?p=235002#p235002
+    ## necessary due to https://github.com/ratanakvlun/deluge-ltconfig/issues/23
     cd /usr/lib/python3/dist-packages
-    ln -s libtorrent.cpython-36dm-x86_64-linux-gnu.so libtorrent.so
+    cat<<EOF | patch -p1
+diff --git a/deluge/log.py b/deluge/log.py
+index 75e8308b5..0f9877fdb 100644
+--- a/deluge/log.py
++++ b/deluge/log.py
+@@ -86,7 +86,7 @@ class Logging(LoggingLoggerClass):
+     def exception(self, msg, *args, **kwargs):
+         yield LoggingLoggerClass.exception(self, msg, *args, **kwargs)
+ 
+-    def findCaller(self, stack_info=False):  # NOQA: N802
++    def findCaller(self, *args, **kwargs):  # NOQA: N802
+         f = logging.currentframe().f_back
+         rv = '(unknown file)', 0, '(unknown function)'
+         while hasattr(f, 'f_code'):
+EOF
 elif [ "$1" = "--v1" ]; then
-    apt-get install -qq wget python-libtorrent python-twisted-core python-openssl python-xdg python-chardet geoip-database
-    wget https://launchpad.net/ubuntu/+archive/primary/+files/deluge-common_1.3.15-2_all.deb https://launchpad.net/ubuntu/+archive/primary/+files/deluged_1.3.15-2_all.deb
-    dpkg -i *.deb
+    ## NOTE: geoip-database is optional
+    apt-get install -qq wget geoip-database
+
+    ## based on https://github.com/josh-gaby/deluge1.3.15-Ubuntu-20.04
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/libt/libtorrent-rasterbar/python-libtorrent_1.1.5-1build1_amd64.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/libt/libtorrent-rasterbar/libtorrent-rasterbar9_1.1.5-1build1_amd64.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/main/b/boost1.65.1/libboost-system1.65.1_1.65.1+dfsg-0ubuntu5_amd64.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/b/boost1.65.1/libboost-python1.65.1_1.65.1+dfsg-0ubuntu5_amd64.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/d/deluge/deluge-common_1.3.15-2_all.deb
+    wget -q --show-progress --progress=bar:force http://security.ubuntu.com/ubuntu/pool/main/t/twisted/python-twisted-core_17.9.0-2ubuntu0.3_all.deb
+    wget -q --show-progress --progress=bar:force http://security.ubuntu.com/ubuntu/pool/main/t/twisted/python-twisted-bin_17.9.0-2ubuntu0.3_amd64.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/main/i/incremental/python-incremental_16.10.1-3_all.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/d/deluge/deluge-common_1.3.15-2_all.deb
+    wget -q --show-progress --progress=bar:force http://archive.ubuntu.com/ubuntu/pool/universe/d/deluge/deluged_1.3.15-2_all.deb
+
+    apt install -qq -y ./*.deb
     rm *.deb
 else
     echo "ERROR: invalid argument" 1>&2
